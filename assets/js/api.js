aa5c90598b06681b4f66863268ef27f6ffc50be0
JoseOtavio-Miguel <=>
2025-10-27 09:29:56 -0300

    Acessing Profile Data from JSON

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function  fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/JoseOtavio-Miguel/js-developer-portfolio/refs/heads/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/JoseOtavio-Miguel/PortifolioJS/refs/heads/main/assets/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
